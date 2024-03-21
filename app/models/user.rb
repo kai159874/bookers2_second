05391dc06419045126d2ac0f7a3eb9b4d6284235
KaichiIwahara <157882052+kai159874@users.noreply.github.com>
2024-03-21 06:08:04 +0000

    [add] guestuser

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,5 +17,18 @@ class User < ApplicationRecord
     end
     profile_image.variant(resize_to_limit: [weight, height]).processed
   end
-
+  
+GUEST_USER_EMAIL = "guest@example.con"
+  
+  def self.guest
+    find_or_create_by!(email: GUEST_USER_EMAIL) do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = "guestuser"
+    end
+  end
+  
+  def guest_user?
+    email == GUEST_USER_EMAIL
+  end
+  
 end
